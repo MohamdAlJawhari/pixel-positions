b20e833a8d84b7e32eabfc72ffd4d6e5cfac88ae
MohamdAlJawhari <mohamd.aljawhari.com>
2025-09-17 15:07:59 +0300

    Add Tailwind CSS configuration file with custom color

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/10 rounded-lg mt-6 flex flex-col text-center">
+
+    <div class="self-start text-sm ">
+        Laracast
+    </div>
+
+    <div class="py-8 font-bold text-xl">
+        <h3>Video Editor</h3>
+        <p>Ful Time - From $50,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Design</x-tag>
+            <x-tag>Product</x-tag>
+        </div>
+
+        <img src="https://placehold.co/42x42" alt="Placeholder" class="rounded-lg">
+    </div>
+</div>
